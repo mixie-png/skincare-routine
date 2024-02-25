11293fee2cdc51959bc6f729fd9d6e4343e60f9d
valerylot <lotvalery@gmail.com>
2024-02-24 20:42:12 -0800

    added the add routine form and CSS styling

--- client/src/views/Footer.jsx
+++ client/src/views/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../assets/static/css/Footer.css'
+import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
     return (
         <div>
             <div className='flex'>
-                <img className='home-icon' src="./src/assets/pics/home.png" alt="home" />
+                <img className='home-icon' src="./src/assets/pics/home.png" alt="home"/>
                 <img className='routine-icon' src="./src/assets/pics/routine.png" alt="routine" />
                 <img className='personal-hygiene-icon' src="./src/assets/pics/personal-hygiene.png" alt="personal-hygiene" />
                 <img className='setting-icon' src="./src/assets/pics/setting.png" alt="setting" />
