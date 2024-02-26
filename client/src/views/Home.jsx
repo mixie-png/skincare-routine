21cd277ffc97a910c3f19a3741e67a3dab0ecb09
Mixie Clerard <clerardm@gmail.com>
2024-02-26 16:26:37 -0500

    removed comments

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -48,8 +48,6 @@ const Home = () => {
                 <Link className='add-routine' to="/create">Add Routine</Link>
             </div>
             {/* this is the footer that shows the logos */}
-            {/* this is the footer that shows the logos */}
-            {/* this is the footer that shows the logos */}
             <Footer></Footer>
         </div>
     )
